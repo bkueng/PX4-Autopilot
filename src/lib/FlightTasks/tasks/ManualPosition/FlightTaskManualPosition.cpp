5856c7ea340d1524474f5b3d8dfc2f6efbdf4fe6
Beat Küng <beat-kueng@gmx.net>
2018-09-24 12:00:35 +0200

    TEST: limit jerk in position control
    also disable position controller.

--- src/lib/FlightTasks/tasks/ManualPosition/FlightTaskManualPosition.cpp
+++ src/lib/FlightTasks/tasks/ManualPosition/FlightTaskManualPosition.cpp
@@ -72,6 +72,7 @@ bool FlightTaskManualPosition::activate()
 	return ret;
 }
 
+#include <mathlib/math/filter/LowPassFilter2p.hpp>
 void FlightTaskManualPosition::_scaleSticks()
 {
 	/* Use same scaling as for FlightTaskManualAltitude */
@@ -112,6 +113,65 @@ void FlightTaskManualPosition::_scaleSticks()
 
 	/* Rotate setpoint into local frame. */
 	_rotateIntoHeadingFrame(vel_sp_xy);
+	/*
+	changed params:
+		Symbols: x = used, + = saved, * = unsaved
+		x + COM_OF_LOSS_T [91,168] : 5.0000
+		x + COM_RC_IN_MODE [99,176] : 2
+		x + COM_RC_LOSS_T [100,177] : 1.0000
+		x + MC_AIRMODE [247,542] : 1
+		x + MC_PITCHRATE_MAX [253,548] : 1200.0000
+		x + MC_PITCHRATE_P [254,549] : 0.2000
+		x + MC_PITCH_P [255,550] : 6.0000
+		x + MC_ROLLRATE_MAX [261,556] : 1200.0000
+		x + MC_ROLLRATE_P [262,557] : 0.2000
+		x + MC_ROLL_P [263,558] : 6.0000
+		x + MC_YAWRATE_MAX [274,569] : 800.0000
+		x + MPC_ACC_HOR [289,600] : 20.0000
+		x + MPC_ACC_HOR_MAX [291,602] : 20.0000
+		x + MPC_HOLD_DZ [296,607] : 0.0000
+		x + MPC_HOLD_MAX_XY [297,608] : 0.0000
+		x + MPC_HOLD_MAX_Z [298,609] : 2.0000
+		x + MPC_POS_MODE [309,621] : 0
+		x + MPC_THR_MIN [312,624] : 0.0000
+		x + MPC_TILTMAX_AIR [313,625] : 70.0000
+		x + MPC_VEL_MANUAL [318,630] : 20.0000
+		x + MPC_XY_VEL_MAX [324,636] : 20.0000
+		x + MPC_Z_VEL_I [331,643] : 0.1500
+		x + MPC_Z_VEL_MAX_DN [332,644] : 5.0000
+		x + MPC_Z_VEL_MAX_UP [333,645] : 5.0000
+		x + MPC_Z_VEL_P [334,646] : 0.6000
+	*/
+	/*
+	Vector2f vel_xy(_velocity(0), _velocity(1));
+	static Vector2f vel_prev = vel_xy;
+	static math::LowPassFilter2p lp_filters[2]{{125, 10}, {125, 10}};
+	Vector2f vel_xy_smooth = Vector2f(lp_filters[0].apply(vel_xy(0)), lp_filters[1].apply(vel_xy(1)));
+	Vector2f accel_diff = (vel_sp_xy - vel_xy_smooth) / _deltatime;
+	Vector2f accel_cur = (vel_xy - vel_prev) / _deltatime;
+	Vector2f jerk_diff = (accel_diff - accel_cur) / _deltatime;
+	float max_jerk = 30000.f;
+
+	// velocity-dependent max jerk
+	float min_jerk_limit_param = 30000.f;
+	if (min_jerk_limit_param > 0.001f) {
+		max_jerk *= sqrtf(vel_xy_smooth.norm());
+	//		max_jerk *= vel_xy_smooth.norm();
+		if (max_jerk < min_jerk_limit_param) max_jerk = min_jerk_limit_param;
+	}
+	// - seems to work basically, velocity-based as well
+	// - problems:
+	//   - steps in attitude sp (or: attitude still changes very fast on certain events like direction change)
+	//   - small twitches in attitude, even with the filter
+
+	//printf("jerk: %.3f\n", (double)jerk_diff.norm());
+	if (jerk_diff.norm_squared() > max_jerk * max_jerk && max_jerk > 0.001f) {
+		jerk_diff = max_jerk * jerk_diff.normalized();
+		vel_sp_xy = (jerk_diff * _deltatime + accel_cur) * _deltatime + vel_xy_smooth;
+	}
+	vel_prev = vel_xy;
+	//*/
+
 	_velocity_setpoint(0) = vel_sp_xy(0);
 	_velocity_setpoint(1) = vel_sp_xy(1);
 }
@@ -142,6 +202,9 @@ void FlightTaskManualPosition::_updateXYlock()
 		_position_setpoint(0) = NAN;
 		_position_setpoint(1) = NAN;
 	}
+
+	_position_setpoint(0) = NAN;
+	_position_setpoint(1) = NAN;
 }
 
 void FlightTaskManualPosition::_updateSetpoints()
