f28273a3b6e8f86998e6f6d6b83e2697646b483e
Beat Küng <beat-kueng@gmx.net>
2021-03-19 09:00:30 +0100

    TEST: github ci: push parameter metadata to s3

--- src/modules/mavlink/streams/COMPONENT_INFORMATION.hpp
+++ src/modules/mavlink/streams/COMPONENT_INFORMATION.hpp
@@ -64,9 +64,9 @@ public:
 		mavlink_component_information_t component_info{};
 		PX4_DEBUG("COMPONENT_INFORMATION request");
 
-		strncpy(component_info.general_metadata_uri, "mftp://etc/extras/component_general.json.xz",
-			sizeof(component_info.general_metadata_uri) - 1);
-		component_info.general_metadata_file_crc = component_information::component_general_crc;
+//		strncpy(component_info.general_metadata_uri, "mftp://etc/extras/component_general.json.xz",
+//			sizeof(component_info.general_metadata_uri) - 1);
+//		component_info.general_metadata_file_crc = component_information::component_general_crc;
 
 		component_info.time_boot_ms = hrt_absolute_time() / 1000;
 		mavlink_msg_component_information_send_struct(_mavlink->get_channel(), &component_info);
